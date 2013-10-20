7e0dbe1dd63c32d336fe5b2d6daa46a919a63bcb
shuxiao <shuxiao@ualberta.ca>
2013-10-20 17:07:33 -0600

    EditorView UML part

---
+++ src/cs/ualberta/ca/beargitandroid/Json.java
@@ -0,0 +1,16 @@
+package cs.ualberta.ca.beargitandroid;
+
+/**
+ * 
+ * @author shuxiao
+ *Have yet to figure it out
+ */
+public class Json {
+	public void JtoS(){
+		
+	}
+	
+	public void StoJ(){
+		
+	}
+}
