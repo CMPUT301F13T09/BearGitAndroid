4bc7d5b1412547ee6ce511d2d6f53d503d6ab10a
Zili Liu <zliu1@cs.ualberta.ca>
2013-10-30 17:23:38 -0600

    db write some code

--- src/cs/ualberta/ca/beargitandroid/Entry.java
+++ src/cs/ualberta/ca/beargitandroid/Entry.java
@@ -1,20 +1,43 @@
 package cs.ualberta.ca.beargitandroid;
 
+import android.content.ContentValues;
+
 public class Entry {
 	
 	private long id;
 	private String title;
 	private String filename;
-	private String destribe; 
+	private String description; 
 	private String date;
-	private String Author;
+	private String author;
+	private int num;
+	
 
 	public Entry(){
 		
 	}
 
+    private void fillEntryValue(){
+    	String title = StitleText.getText.toString();
+    	String filename = SfilenameText.getText.toString();
+    	String description = SdescriptionText.getText.toString();
+    	String date = SdateText.getText.toString();
+    	String author = sauthorText.getText.toString();
+    			
+    	
+    }
+	
+    public long createNote(String title, String filename, String description, String date, String Author)
+   {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_TITLE, title);
+        initialValues.put(KEY_FILENAME, filename);
+        initialValues.put(KEY_DATE, date);
+        initialValues.put(KEY_DESCRIPTION, description);
+        initialValues.put(KEY_AUTHOR,author);
+        
 
-	
-	
+        return mDb.insert(DATABASE_TABLE, null, initialValues);
+    }
 	
 }
