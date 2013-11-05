64eca41878a12fbd717d03daa07c9744393901af
shuxiao <shuxiao@ualberta.ca>
2013-11-05 12:53:42 -0700

    XML Files

--- gen/cs/ualberta/ca/beargitandroid/R.java
+++ gen/cs/ualberta/ca/beargitandroid/R.java
@@ -24,10 +24,49 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int MyStory=0x7f08000f;
+        public static final int action_settings=0x7f08001e;
+        public static final int addchapter=0x7f080008;
+        public static final int addmedia=0x7f08000a;
+        public static final int button1=0x7f08000b;
+        public static final int create=0x7f080014;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080007;
+        public static final int error=0x7f080012;
+        public static final int fragment=0x7f080016;
+        public static final int imageView1=0x7f080018;
+        public static final int listView1=0x7f080003;
+        public static final int local=0x7f08000d;
+        public static final int next=0x7f08001d;
+        public static final int radio0=0x7f08001a;
+        public static final int radio1=0x7f08001b;
+        public static final int radio2=0x7f08001c;
+        public static final int radioGroup1=0x7f080019;
+        public static final int reload=0x7f080013;
+        public static final int remote=0x7f08000e;
+        public static final int save=0x7f080009;
+        public static final int save2=0x7f080004;
+        public static final int searchView1=0x7f08000c;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080006;
+        public static final int textView4=0x7f080015;
+        public static final int title=0x7f080005;
+        public static final int useless1=0x7f080010;
+        public static final int useless2=0x7f080011;
+        public static final int videoView1=0x7f080017;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int add_link=0x7f030001;
+        public static final int create_story=0x7f030002;
+        public static final int edit_chapter=0x7f030003;
+        public static final int home_screen_error=0x7f030004;
+        public static final int home_screen_local=0x7f030005;
+        public static final int home_screen_mystory=0x7f030006;
+        public static final int home_screen_remote=0x7f030007;
+        public static final int story_description=0x7f030008;
+        public static final int story_fragment=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f070000;
