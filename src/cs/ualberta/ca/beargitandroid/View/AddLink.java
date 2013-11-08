a87e04b0d658e4f826de05004792dbbbbd6a4c44
shuxiao <shuxiao@ualberta.ca>
2013-11-07 18:20:29 -0700

    connect each viewers

---
+++ src/cs/ualberta/ca/beargitandroid/View/AddLink.java
@@ -0,0 +1,34 @@
+package cs.ualberta.ca.beargitandroid.View;
+
+import java.util.ArrayList;
+
+import cs.ualberta.ca.beargitandroid.Story;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddLink extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_link);
+		
+		Button saveButton = (Button) findViewById(R.id.save2);
+		
+		saveButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				//The place you add the code that save the link to the database
+				finish();
+			}
+		});
+		
+	}
+}
+
