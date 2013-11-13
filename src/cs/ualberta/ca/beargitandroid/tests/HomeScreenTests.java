20ca4163997f45e80cd2139b91567c077e116ae3
Zili Liu <zliu1@cs.ualberta.ca>
2013-11-13 16:46:26 -0700

    test

--- src/cs/ualberta/ca/beargitandroid/tests/HomeScreenTests.java
+++ src/cs/ualberta/ca/beargitandroid/tests/HomeScreenTests.java
@@ -1,8 +1,21 @@
 package cs.ualberta.ca.beargitandroid.tests;
 
 import junit.framework.Assert;
+import android.R;
 import android.test.AndroidTestCase;
+import cs.ualberta.ca.beargitandroid.Story;
+import cs.ualberta.ca.beargitandroid.View.ViewStory;
+import cs.ualberta.ca.beargitandroid.View.homeScreenLocal;
 import cs.ualberta.ca.beargitandroid.controller.*;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 // TODO: Auto-generated Javadoc
 
 /**
@@ -15,13 +28,46 @@ import cs.ualberta.ca.beargitandroid.controller.*;
  * functions.
  */
 public class HomeScreenTests extends AndroidTestCase {
+	private TextView storyTitle;
+	private TextView storyAuthor;
+	private TextView storyDecription;
+	private Story story;
+	private Context cxt;
+	private long id;
+	private Intent intent;
 	
+	public HomeScreenTests(){
+		super();
+	}
+	
+	protected void setUp() throws Exception{
+		super.setUp();
+		this.story = new Story (this.cxt,this.id);
+		this.story.createNewStory("title","description","author");
+		Intent intent = new Intent();
+		intent.putExtra("storyID", story.getStoryID());
+		
+		
+		
+	}
+	
+	public void test(){
+		
+		storyTitle = (TextView) Activity.findViewById(R.id.title3);
+		storyAuthor = (TextView) Activity.findViewById(R.id.descriptionContent);
+		storyDecription = (TextView)Activity.findViewById(R.id.title2);
+		
+		 
+          assertTrue(storyTitle != null);
+          assertTrue(storyAuthor != null);
+          assertTrue(storyDecription != null);
+
+		
+	}
     /**
      * Test_ home screen.
      *
      * @throws Throwable the throwable
      */
-    public void test_HomeScreen() throws Throwable {
-       Assert.assertTrue(true);
-    }
+    
 }
