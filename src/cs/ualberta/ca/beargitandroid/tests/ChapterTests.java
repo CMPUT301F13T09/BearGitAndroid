c59086189cebd45ba2f3769176a75cd8e9bf29bb
weiyu1 <weiyu1@ualberta.ca>
2013-11-13 20:23:16 -0700

    Update ChapterTests.java

--- src/cs/ualberta/ca/beargitandroid/tests/ChapterTests.java
+++ src/cs/ualberta/ca/beargitandroid/tests/ChapterTests.java
@@ -15,9 +15,24 @@ import cs.ualberta.ca.beargitandroid.*;
 public class ChapterTests extends AndroidTestCase {
 	
 	private Chapter chapter;
-	private String chapter = "some chapter";
 	private String context = "some context";
 	private String option = "some option";
+	public boid setUp() throws Exception{
+		chapter = new Chapter(context,option);
+	}
+	public void test ChangeAutherAndComment()
+	{
+		String newContext = "Context";
+		String newOption = "option";
+		chapter.setContext(newContext());
+		chapter.setOption(newOption);
+		//assertTRues(res);
+		assertEquals(newContext,chapter.getContext());
+		assertEquals(newOption,chapter.getOption());
+		
+	}
+	
+	
     public void test_Chapter() throws Throwable {
        assertEquals(chapter,Chapter.getChapter());
     }
