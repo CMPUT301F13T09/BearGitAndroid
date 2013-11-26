90014b6214943df295c71ff3dd41fbcd81b0b2cd
Zili Liu <zliu1@cs.ualberta.ca>
2013-11-26 11:47:13 -0700

    controller modify

--- src/cs/ualberta/ca/beargitandroid/controller/chapterEditer.java
+++ src/cs/ualberta/ca/beargitandroid/controller/chapterEditer.java
@@ -1,23 +1,85 @@
 package cs.ualberta.ca.beargitandroid.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import cs.ualberta.ca.beargitandroid.Chapter;
+import cs.ualberta.ca.beargitandroid.Story;
+import cs.ualberta.ca.beargitandroid.View.R;
 import android.app.Activity;
+import android.content.Context;
+import android.widget.SimpleAdapter;
 
 
 public class chapterEditer extends Activity {
 	
 	static long ID;
-	private Chapter chapter[];
 	
+    private static long id;
+	
+	/** The name. */
+	private String name;
+	
+	/** The context. */
+	private String context;
+	
+	/** The option. */
+	private ArrayList< HashMap<String,String> > option;
+	
+	/** The media. */
+	private String media;
+	
+	/** The story. */
+	private Story story;
+	
+	private Chapter chapter;
+	
+	private Context cxt;
 	/**
 	 * Chapter.
 	 */
-	public void chapter(){
+
+	public chapterEditer(Context context, Story story){
+		 
+		this.cxt = context;
+		this.story = story;
 		
 	}
-	public void chapterShow(){
+	
+	public SimpleAdapter showchapter(){
+		
+		ArrayList<HashMap< String , String >> map = story.getChapterList(this.id);
+		
+		if(map == null){
+			return null;
+		}
+		String[] from = new String[]{"title"};
+		int[] to = new int[] {R.id.title2};
+		SimpleAdapter ad = new SimpleAdapter(this.cxt,map,R.layout.story_list_elem,from,to);
+		
+		
+		return ad;
+		
 		
 	}
+	
+	public SimpleAdapter showoption(){
+		
+		HashMap<String,String> map = (HashMap<String,String>) option.get((int) id);
+		
+		if(map == null){
+			
+			return null;
+			
+		}
+		
+		String from = new String[]{title};
+		int[] to = new int[]{R.id.title6};
+		SimpleAdapter op = new SimpleAdapter(this.cxt,map,R.layout.story_list_elem,from,to);
+		
+	}
+	
+	
 	public void editOption( ){
 		
 	}
--- src/cs/ualberta/ca/beargitandroid/controller/editOption.java
+++ src/cs/ualberta/ca/beargitandroid/controller/editOption.java
@@ -1,12 +1,25 @@
 package cs.ualberta.ca.beargitandroid.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.graphics.BitmapFactory.Options;
+import android.widget.SimpleAdapter;
 import cs.ualberta.ca.beargitandroid.Chapter;
+import cs.ualberta.ca.beargitandroid.Story;
+import cs.ualberta.ca.beargitandroid.View.R;
 
 
 public class editOption {
 
     private Chapter chapter;
-
+    
+    private Context cxt;
+    
+    private static long id;
+    
+    private ArrayList< HashMap<String,String> > option;
 	/**
 	 * Show link.
 	 */
@@ -19,5 +32,28 @@ public class editOption {
     public void delete(){
     	
     }
-
+    public editOption(Context context, long id){
+    	
+    	this.cxt = context;
+        
+    	
+    }
+    
+    public SimpleAdapter showoption(){
+		
+		HashMap<String,String> map = (HashMap<String,String>) option.get((int) id);
+		
+		if(map == null){
+			
+			return null;
+			
+		}
+		
+		String opt = new String[]{option.get(index)};
+		int[] to = new int[]{R.id.title6};
+		SimpleAdapter op = new SimpleAdapter(this.cxt,map,R.layout.story_list_elem,from,to);
+		
+	}
+	
+		
 }
